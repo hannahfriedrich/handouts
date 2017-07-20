e3169ca7522028b7e6ca72cc1fd0311d1a03c587
hannahfriedrich <hannahfriedrich14@gmail.com>
2017-07-20 18:13:36 +0000

    my github page

--- worksheet-2.sh
+++ worksheet-2.sh
@@ -2,13 +2,13 @@
 
 ## Configure git
 
-git config --global user.name ...
-git config --global user.email ...
+git config --global user.name hannahfriedrich
+git config --global user.email hannahfriedrich14@gmail.com
 
 ## Change the "origin" remote URL and push
-git remote set-url origin ...
-git push --all
+git remote set-url origin git@github.com:hannahfriedrich/handouts.git
+
 
 ## Set upstream
-#git remote add upstream https://github.com/sesync-ci/handouts
-#git pull upstream master
+git remote add upstream https://github.com/sesync-ci/handouts
+git pull upstream master
